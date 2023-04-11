370ad95a1f9a9efcb030f7d83f6ecca362bea00c
Pinkyboi <ayman.benaissa2000@gmail.com>
2023-04-11 01:38:29 +0000

    Remove useless check on buffer size

--- src/recv.c
+++ src/recv.c
@@ -13,21 +13,17 @@ static void print_trace_info(struct sockaddr_in *socket_addr)
     printf("  %.3f ms", GET_RTT());
 }
 
-bool is_packet_mine(char *recv_buffer, uint16_t packet_size)
+bool is_packet_mine(char *recv_buffer)
 {
     struct iphdr*   ip_hdr;
     struct icmphdr* icmp_hdr;
     struct iphdr*   inner_ip_hdr;
     struct icmphdr* inner_icmp_hdr;
 
-    if (packet_size < (sizeof(struct iphdr) + sizeof(struct icmphdr)))
-        return false;
     ip_hdr = (struct iphdr*)recv_buffer;
     icmp_hdr = (struct icmphdr *)(recv_buffer + (ip_hdr->ihl << 2));
     if (icmp_hdr->type == ICMP_TIME_EXCEEDED)
     {
-        if (packet_size < 2 * (sizeof(struct iphdr) + sizeof(struct icmphdr)))
-            return false;
         inner_ip_hdr = (struct iphdr*)((void *)icmp_hdr + sizeof(struct icmphdr));
         inner_icmp_hdr = (struct icmphdr*)((void *)inner_ip_hdr + (inner_ip_hdr->ihl << 2));
         if (inner_icmp_hdr->type == ICMP_ECHO && inner_icmp_hdr->un.echo.id == my_htons(getpid()))
@@ -56,7 +52,7 @@ void recv_probe(uint8_t prob_number)
         {
             if (recvfrom(g_traceroute.sockfd, recv_buffer, IP_MAXPACKET, 0, &sender_addr, &addrlen) > 0)
             {
-                if (is_packet_mine(recv_buffer, IP_MAXPACKET))
+                if (is_packet_mine(recv_buffer))
                 {
                     g_traceroute.prob_time.r_time = get_timeval();
                     print_trace_info((struct sockaddr_in *)&sender_addr);
